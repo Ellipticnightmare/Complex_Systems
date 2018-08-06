32960c2abdb8aa336a21df32e3b3b1e1612a5ad2
Ellipticnightmare <31974144+Ellipticnightmare@users.noreply.github.com>
2018-08-06 15:50:59 +1000

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Move : NetworkBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody rigid;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+            Vector3 force = new Vector3(inputH, 0, inputV);
+
+            force = transform.TransformDirection(force);
+
+            rigid.AddForce(force * speed);
+        }
+    }
+}
